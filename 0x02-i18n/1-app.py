319c0af453237d6ee0def9977a50a86ab19821c9
ManNjoro <elijohnmwoho@gmail.com>
2024-01-09 10:55:27 +0300

    Add Babel configuration and a route to render a template.

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,7 +2,7 @@
 """
 """
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_babel import Babel
 
 
@@ -17,7 +17,15 @@ class Config(object):
     LANGUAGES = ["en", "fr"]
 
 
+app.config['BABEL_DEFAULT_LOCALE'] = Config.LANGUAGES[0]
+app.config['BABEL_DEFAULT_TIMEZONE'] = 'UTC'
 app.config.from_object(Config)
 
-babel.default_locale = 'en'
-babel.timezone = 'UTC'
+
+@app.route('/')
+def hello():
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)

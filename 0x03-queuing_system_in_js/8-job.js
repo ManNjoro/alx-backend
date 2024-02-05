361931041360c532dcd61176c299bd09d9d8db03
ManNjoro <elijohnmwoho@gmail.com>
2024-02-05 19:22:12 +0300

    Renamed function and changed export statement. Changed job creation to use `for..of` loop. Changed event names to match the queue library. Added `save()` call after job creation.

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -1,26 +1,30 @@
-export default function createPushNotificationsJobs(jobs, queue) {
-  if (!Array.isArray(jobs)) {
-    throw Error(`${jobs} is not an array`);
+export const createPushNotificationsJobs = (jobs, queue) => {
+  if (!(jobs instanceof Array)) {
+    throw new Error("Jobs is not an array");
   }
-  jobs.forEach((element) => {
-    const job = queue
-      .create("push_notification_code_3", element)
-      .save((err) => {
-        if (!err) {
-          console.log(`Notification job created: ${job.id}`);
-        }
-      });
+  for (const jobInfo of jobs) {
+    const job = queue.create("push_notification_code_3", jobInfo);
 
-      job.on("complete", () => {
-        console.log(`Notification job ${job.id} completed`);
+    job
+      .on("enqueue", () => {
+        console.log("Notification job created:", job.id);
+      })
+      .on("complete", () => {
+        console.log("Notification job", job.id, "completed");
+      })
+      .on("failed", (err) => {
+        console.log(
+          "Notification job",
+          job.id,
+          "failed:",
+          err.message || err.toString()
+        );
+      })
+      .on("progress", (progress, _data) => {
+        console.log("Notification job", job.id, `${progress}% complete`);
       });
-    
-      job.on("failed", (err) => {
-        console.log(`Notification job ${job.id} failed: ${err}`);
-      });
-    
-      job.on("progress", (progress, _data) => {
-        console.log(`Notification job ${job.id} ${progress}% complete`);
-      });
-  });
-}
+    job.save();
+  }
+};
+
+export default createPushNotificationsJobs;
